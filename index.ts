5f422700271bf858e7893d2eb33cf65f3551ad6b
Anwesh43 <anweshthecool0@gmail.com>
2021-04-18 17:34:31 +0530

    adding Stage class

--- index.ts
+++ index.ts
@@ -66,3 +66,21 @@ class DrawingUtil {
         DrawingUtil.drawBarBallReflector(context, scale)
     }
 }
+
+class Stage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D
+
+    initCanvas() {
+        this.canvas.width = w 
+        this.canvas.height = h 
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = backColor 
+        this.context.fillRect(0, 0, w, h)
+    }
+}
