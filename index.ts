4623f85737f662ab560503311325cd6df233eeb8
Anwesh43 <anweshthecool0@gmail.com>
2021-04-18 17:22:41 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -12,6 +12,8 @@ const colors : Array<string> = [
     "#BF360C"
 ]
 const backColor : string = "#bdbdbd"
+const barFactor : number = 5.9 
+const rFactor : number = 15.9 
 
 class ScaleUtil {
 
@@ -25,5 +27,42 @@ class ScaleUtil {
 
     static sinify(scale : number) : number {
         return Math.sin(scale * Math.PI)
+    }
+}
+
+class DrawingUtil {
+
+    static drawCircle(context : CanvasRenderingContext2D, x : number, y : number, r : number) {
+        context.beginPath()
+        context.arc(x, y, r, 0, 2 * Math.PI)
+        context.fill()
+    }
+
+    static drawBarBallReflector(context : CanvasRenderingContext2D, scale : number) {
+        const sf : number = ScaleUtil.sinify(scale)
+        const sf1 : number = ScaleUtil.divideScale(sf, 0, parts)
+        const sf2 : number = ScaleUtil.divideScale(sf, 1, parts)
+        const sff2 : number = ScaleUtil.sinify(sf2)
+        const size : number = w / barFactor 
+        const r : number = Math.min(w, h) / rFactor 
+        context.save()
+        DrawingUtil.drawCircle(
+            context, 
+            r + (w - 2 * r) * sf2,
+            r + (h - 3 * r) * sff2,
+            r * sf1 
+        )
+        context.fillRect(
+            w - size * sf1 - (w / 2 - size / 2) * sff2,
+            h - r, 
+            size * sf1, 
+            r  
+        )
+        context.restore()
+    }
+
+    static drawBBRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.fillStyle = colors[i]
+        DrawingUtil.drawBarBallReflector(context, scale)
     }
 }
